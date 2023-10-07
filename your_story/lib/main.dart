dcb3e51b48458c076e0e13c6e0badec49ce0d039
hendjsh <hendjsh1@gmail.com>
2023-10-07 19:01:27 +0300

    test

--- your_story/lib/main.dart
+++ your_story/lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'TECHقصـ',
-     debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false,
       home: HomeScreen(),
     );
   }
