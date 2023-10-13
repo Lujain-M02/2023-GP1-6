4beed78adc6f6c3ba766b73484c02bd39b74a75c
hendjsh <hendjsh1@gmail.com>
2023-10-14 02:46:58 +0300

    title validation, to be just 1 line TextFormField

--- your_story/lib/pages/create_story_pages/create_story_title.dart
+++ your_story/lib/pages/create_story_pages/create_story_title.dart
@@ -1,6 +1,70 @@
 import 'package:flutter/material.dart';
 
 class CreateStoryTitle extends StatelessWidget {
+  const CreateStoryTitle({Key? key, required this.titleController})
+      : super(key: key);
+
+  final TextEditingController titleController;
+
+  String? validateTitle(String? value) {
+    if (value == null || value.trim().isEmpty) {
+      return "الرجاء إدخال العنوان";
+    } else if (!RegExp(r'^[ء-ي\s]+$').hasMatch(value)) {
+      return "العنوان يجب أن يكون عنوان قصتك باللغة العربية فقط";
+    }
+    return null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        // Hint Card
+        const Card(
+          color: Colors.transparent,
+          elevation: 0,
+          child: ListTile(
+            horizontalTitleGap: -15,
+            contentPadding: EdgeInsets.symmetric(horizontal: 0.0),
+            leading: Icon(
+              Icons.lightbulb,
+              color: Colors.amber,
+              size: 20,
+            ),
+            title: Text(
+              "أبق عنوانك قصيرًا، معبرًا وواضحًا!",
+              style: TextStyle(fontSize: 14),
+            ),
+          ),
+        ),
+
+        // Text Form Field
+        Form(
+          autovalidateMode: AutovalidateMode.always,
+          child: Container(
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.grey),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: TextFormField(
+              autovalidateMode: AutovalidateMode.always,
+              controller: titleController,
+              maxLines: null,
+              decoration: const InputDecoration(
+                labelText: "أدخل العنوان هنا",
+                contentPadding: EdgeInsets.all(10),
+              ),
+              validator: validateTitle,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+
+/*class CreateStoryTitle extends StatelessWidget {
   const CreateStoryTitle({super.key, required this.titleController});
 
   final TextEditingController titleController;
@@ -61,7 +125,7 @@ class CreateStoryTitle extends StatelessWidget {
       ],
     );
   }
-}
+}*/
 
 /*class _CreateStoryTitle extends State<CreateStoryTitle> {
   final formKey = GlobalKey<FormState>();
