1cf5ad4827e575ecfdfae159bef977b8bb00e55e
Lujain-M02 <ilujain321@gmail.com>
2023-10-21 13:48:30 +0300

    just file format

--- your_story/lib/startScreen.dart
+++ your_story/lib/startScreen.dart
@@ -1,13 +1,9 @@
-
-
 // ignore_for_file: prefer_const_constructors
 
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 import 'package:your_story/pages/intro_pages.dart';
-
-
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -17,26 +13,26 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => MyHomePage()));
+    });
+  }
 
   @override
- void initState() {
-    super.initState();
-    Future.delayed(const Duration(seconds: 3), (){
-Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=> MyHomePage()));    }
-    );
- }
-   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.transparent, 
+      backgroundColor: Colors.transparent,
       body: Stack(
         children: [
-         
           Container(
             decoration: const BoxDecoration(
               image: DecorationImage(
-                image: AssetImage('assets/background1.png'), 
-                fit: BoxFit.cover, 
+                image: AssetImage('assets/background1.png'),
+                fit: BoxFit.cover,
               ),
             ),
           ),
