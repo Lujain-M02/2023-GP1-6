3d171cea068ef8d976b905b282b429fe8f06001e
Lujain-M02 <ilujain321@gmail.com>
2023-11-11 00:24:53 +0300

    delete story doonneeee

--- your_story/lib/pages/my_stories_page.dart
+++ your_story/lib/pages/my_stories_page.dart
@@ -1,28 +1,88 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:your_story/alerts.dart';
 
-class MyStories extends StatelessWidget {
-final user = FirebaseAuth.instance.currentUser!.uid;
+class MyStories extends StatefulWidget {
+  const MyStories({super.key});
 
-// Query the "stories" subcollection
-//QuerySnapshot storiesQuery =  await userRef.collection('stories').get();
+  @override
+  State<MyStories> createState() => _MyStories();
+}
 
-// Iterate through the documents in the subcollectio
+class _MyStories extends State<MyStories> {
+  List<QueryDocumentSnapshot> stories = [];
+  final user = FirebaseAuth.instance.currentUser!.uid;
+    bool isLoaded = false; // Add a boolean variable
+
+  getStoried() async {
+    QuerySnapshot querySnapshot = await FirebaseFirestore.instance
+        .collection("User")
+        .doc(user)
+        .collection("Stories")
+        .get();
+    stories.addAll(querySnapshot.docs);
+    setState(() {
+      isLoaded = true;
+    });
+
+  }
+
+  deleteStory(int i){
+    ConfirmationDialog.show(context, "عملية الحذف نهائية هل أنت متأكد؟", ()async{
+       await FirebaseFirestore.instance
+        .collection("User")
+        .doc(user)
+        .collection("Stories").doc(stories[i].id).delete();
+            setState(() {
+      stories.removeAt(i); // Remove the deleted item from 'stories'
+    });
+    Navigator.pop(context); // Close the dialog
+    });
+
+
+  }
+
+  @override
+  void initState() {
+    getStoried();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Directionality(textDirection: TextDirection.rtl
-      , child: Container(
-        color: Colors.amber,
-        child: Text(
-          "hi"
-          ,style: TextStyle(color: Colors.black,fontSize: 40),
-        ),
-      )),
+      child: Directionality(
+          textDirection: TextDirection.rtl,
+          child: !isLoaded
+          ? Center(
+                child: CircularProgressIndicator(), // or any loading widget
+          )
+          : stories.isEmpty
+          ? Center(
+            child: Text(
+                  "يبدو أنه لا يوجد لديك قصص\nانتقل للصفحة الرئيسية وأبدأ صناعة قصتك الآن",
+                  textAlign: TextAlign.center,
+                ),
+          )
+          : ListView.builder(
+              itemCount: stories.length,
+              itemBuilder: (context, i) {
+                return Container(
+                  color: Color.fromARGB(255, 187, 208, 238),
+                  margin: EdgeInsets.all(10),
+                  child: ListTile(
+                        leading: Image.asset("assets/white.png") ,
+                        title: Text("${stories[i]['title']}") ,
+                        subtitle: Text("${stories[i]['content']}",maxLines: 1,),
+                        trailing: IconButton(onPressed: (){
+                          deleteStory(i);
+                        }
+                        , icon: const Icon(Icons.delete)),
+                      ),
+                );
+              })
+          ),
     );
-    
   }
-
 }
