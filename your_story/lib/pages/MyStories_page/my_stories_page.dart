cbe15afae341b2c4e75a4c53c5db68dfc89d1db1
Lujain-M02 <ilujain321@gmail.com>
2023-11-20 23:09:15 +0300

    show message after deleting

--- your_story/lib/pages/MyStories_page/my_stories_page.dart
+++ your_story/lib/pages/MyStories_page/my_stories_page.dart
@@ -15,7 +15,7 @@ class MyStories extends StatefulWidget {
 class _MyStories extends State<MyStories> {
   List<QueryDocumentSnapshot> stories = [];
   final user = FirebaseAuth.instance.currentUser!.uid;
-    bool isLoaded = false; // Add a boolean variable
+  bool isLoaded = false; // Add a boolean variable
 
   getStoried() async {
     QuerySnapshot querySnapshot = await FirebaseFirestore.instance
@@ -27,22 +27,38 @@ class _MyStories extends State<MyStories> {
     setState(() {
       isLoaded = true;
     });
-
   }
 
-  deleteStory(int i){
-    ConfirmationDialog.show(context, "عملية الحذف نهائية هل أنت متأكد؟", ()async{
-       await FirebaseFirestore.instance
-        .collection("User")
-        .doc(user)
-        .collection("Stories").doc(stories[i].id).delete();
-            setState(() {
-      stories.removeAt(i); // Remove the deleted item from 'stories'
-    });
-    Navigator.pop(context); // Close the dialog
-    });
 
+  void deleteStory(int i) {
+    ConfirmationDialog.show(context, "عملية الحذف نهائية هل أنت متأكد؟",
+        () async {
+      try {
+        await FirebaseFirestore.instance
+            .collection("User")
+            .doc(user)
+            .collection("Stories")
+            .doc(stories[i].id)
+            .delete();
 
+        setState(() {
+          stories.removeAt(i); // Remove the deleted item from 'stories'
+        });
+        ScaffoldMessenger.of(context).showSnackBar(
+          CustomSnackBar(
+            content: 'تم الحذف بنجاح',
+          ),
+        );
+      } catch (e) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          CustomSnackBar(
+            content: ' حدث خطأ اثناء الحذف ',
+          ),
+        );
+      }
+
+      Navigator.pop(context); // Close the dialog
+    });
   }
 
   @override
@@ -54,19 +70,16 @@ class _MyStories extends State<MyStories> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Directionality(
-          textDirection: TextDirection.rtl,
-          child:Scaffold(
-            appBar: AppBar(
+        child: Directionality(
+      textDirection: TextDirection.rtl,
+      child: Scaffold(
+          appBar: AppBar(
             title: const Text(
               "قصصي",
-              style: TextStyle(
-                  fontSize: 25,
-                  color: Colors.black), 
+              style: TextStyle(fontSize: 25, color: Colors.black),
             ),
-            centerTitle: true, 
-            backgroundColor:
-                Colors.white, 
+            centerTitle: true,
+            backgroundColor: Colors.white,
           ),
           floatingActionButton: FloatingActionButton(
             onPressed: () {
@@ -79,52 +92,71 @@ class _MyStories extends State<MyStories> {
             backgroundColor: const Color.fromARGB(255, 15, 26, 107),
             child: const Icon(Icons.add),
           ),
-            body:!isLoaded
-          ?  Center(
-                child: CircularProgressIndicator(color: YourStoryStyle.titleColor,),
-          )
-          : stories.isEmpty
-          ? const Center(
-            child: Text(
-                  "يبدو أنه لا يوجد لديك قصص\nانتقل للصفحة الرئيسية وأبدأ صناعة قصتك الآن",
-                  textAlign: TextAlign.center,
-                ),
-          )
-          : ListView.builder(
-              itemCount: stories.length,
-              itemBuilder: (context, i) {
-                return Container(
-                                    height: 120,
-                  margin: const EdgeInsets.all(12),
-                  decoration: const BoxDecoration(
-                    color: Color.fromARGB(255, 187, 208, 238) ,
-                    borderRadius: BorderRadius.all(Radius.circular(10))
+          body: !isLoaded
+              ? Center(
+                  child: CircularProgressIndicator(
+                    color: YourStoryStyle.titleColor,
                   ),
-                  child: Center(
-                    child: ListTile(
-                          onTap: () {
-                            showCustomModalBottomSheet(context, Directionality(
-                              textDirection:TextDirection.rtl ,
-                              child: Column(children: [
-                                Center(child: Text(stories[i]['title'],style: const TextStyle(fontSize: 25),)),
-                                const SizedBox(height: 8,),
-                                Text(stories[i]['content'])
-                              ],),
-                            ));
-                          },
-                          leading: Image.asset("assets/white.png") ,
-                          title: Text("${stories[i]['title']}",style: const TextStyle(fontSize: 25),) ,
-                          subtitle: Text("${stories[i]['content']}" ,style: const TextStyle(fontSize: 20),maxLines: 1,),
-                          trailing: IconButton(onPressed: (){
-                            deleteStory(i);
-                          }
-                          , icon: const Icon(Icons.delete)),
-                        ),
-                  ),
-
-                );
-              })
-          ),
+                )
+              : stories.isEmpty
+                  ? const Center(
+                      child: Text(
+                        "يبدو أنه لا يوجد لديك قصص\nانتقل للصفحة الرئيسية وأبدأ صناعة قصتك الآن",
+                        textAlign: TextAlign.center,
+                      ),
+                    )
+                  : ListView.builder(
+                      itemCount: stories.length,
+                      itemBuilder: (context, i) {
+                        return Container(
+                          height: 120,
+                          margin: const EdgeInsets.all(12),
+                          decoration: const BoxDecoration(
+                              color: Color.fromARGB(255, 187, 208, 238),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10))),
+                          child: Center(
+                            child: ListTile(
+                              onTap: () {
+                                showCustomModalBottomSheet(
+                                    context,
+                                    Directionality(
+                                      textDirection: TextDirection.rtl,
+                                      child: Column(
+                                        children: [
+                                          Center(
+                                              child: Text(
+                                            stories[i]['title'],
+                                            style:
+                                                const TextStyle(fontSize: 25),
+                                          )),
+                                          const SizedBox(
+                                            height: 8,
+                                          ),
+                                          Text(stories[i]['content'])
+                                        ],
+                                      ),
+                                    ));
+                              },
+                              leading: Image.asset("assets/white.png"),
+                              title: Text(
+                                "${stories[i]['title']}",
+                                style: const TextStyle(fontSize: 25),
+                              ),
+                              subtitle: Text(
+                                "${stories[i]['content']}",
+                                style: const TextStyle(fontSize: 20),
+                                maxLines: 1,
+                              ),
+                              trailing: IconButton(
+                                  onPressed: () {
+                                    deleteStory(i);
+                                  },
+                                  icon: const Icon(Icons.delete)),
+                            ),
+                          ),
+                        );
+                      })),
     ));
   }
 }
