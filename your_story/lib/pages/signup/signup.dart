81e3da7f1589e8e8054bf2b0a3216051e2b15786
Lujain-M02 <ilujain321@gmail.com>
2023-11-28 00:01:11 +0300

    modify if statment .trim()

--- your_story/lib/pages/signup/signup.dart
+++ your_story/lib/pages/signup/signup.dart
@@ -132,7 +132,7 @@ class _SignUpState extends State<SignUp> {
                                             return " يجب أن يحتوي الاسم أكثر من حرف على الأقل";
                                           } else if (!RegExp(
                                                   r"^[\u0600-\u065F\u066A-\u06EF\u06FA-\u06FFa-zA-Z]+(?:\s[\u0600-\u065F\u066A-\u06EF\u06FA-\u06FFa-zA-Z]+)?$")
-                                              .hasMatch(value)) {
+                                              .hasMatch(value.trim())) {
                                             return 'أدخل اسم يحتوي على أحرف فقط';
                                           }
                                           return null;
