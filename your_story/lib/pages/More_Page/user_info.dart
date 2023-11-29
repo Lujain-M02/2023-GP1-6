478a38f5214df9f0e8e227c481ed3f66083d9f8d
nourahmaziad <nourahmaziad@gmail.com>
2023-11-30 00:06:13 +0300

    update in user info

--- your_story/lib/pages/More_Page/user_info.dart
+++ your_story/lib/pages/More_Page/user_info.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:your_story/alerts.dart';
 import 'package:your_story/style.dart';
-
 class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -53,40 +52,34 @@ class _ProfileInformationState extends State<ProfileInformation> {
   }
 
   Widget customListTile(BuildContext context, IconData icon, String title, String subtitle, String field) {
-    
-    return InkWell(
-      onTap: () {
-        if (field == 'email') {
-          // Navigate to edit email page
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => EditEmailPage()),
-          );
-        } else if (field == 'username') {
-          // Navigate to edit username page
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => EditUsernamePage()),
-          );
-        }
-      },
-      child: Card(
-        color: Colors.white,
-        child: ListTile(
-          leading: Icon(icon, color: const Color.fromARGB(255, 15, 26, 107)),
+  return InkWell(
+    onTap: () {
+      if (field == 'username') {
+        // Navigate to edit username page
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => EditUsernamePage()),
+        );
+      }
+    },
+    child: Card(
+      color: Colors.white,
+      child: ListTile(
+        leading: Icon(icon, color: const Color.fromARGB(255, 15, 26, 107)),
         title: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Text(title),
-            const Icon(Icons.arrow_forward_ios, size: 16, color: Color.fromARGB(255, 164, 161, 161)), 
+            if (field == 'username') // Add arrow icon only for the 'username' field
+              const Icon(Icons.arrow_forward_ios, size: 16, color: Color.fromARGB(255, 164, 161, 161)),
           ],
-        ),   
-          subtitle: 
-              Text(subtitle),
-               ),
+        ),
+        subtitle: Text(subtitle),
       ),
-    );
-  }
+    ),
+  );
+}
+
 
   @override
   Widget build(BuildContext context) {
@@ -94,9 +87,9 @@ class _ProfileInformationState extends State<ProfileInformation> {
       future: fetchUserInfo(),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(
+          return Center(
             child: CircularProgressIndicator(
-              color: Colors.blue,
+              color: YourStoryStyle.titleColor,
             ),
           );
         } else if (snapshot.hasError || !snapshot.hasData) {
@@ -125,113 +118,13 @@ class _ProfileInformationState extends State<ProfileInformation> {
   }
 }
 
-
-class EditEmailPage extends StatefulWidget {
-  @override
-  _EditEmailPageState createState() => _EditEmailPageState();
-}
-
-class _EditEmailPageState extends State<EditEmailPage> {
-  final TextEditingController _emailController = TextEditingController();
-  final String userId = FirebaseAuth.instance.currentUser!.uid;
-
-  @override
-  Widget build(BuildContext context) {
-    return Directionality(
-      textDirection: TextDirection.rtl,
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          title: const Text(
-            'اعادة تعيين البريد الالكتروني',
-            style: TextStyle(color: Colors.black),
-          ),
-          titleTextStyle: const TextStyle(fontSize: 24),
-          centerTitle: true,
-          leading: const BackButton(color: Colors.black),
-        ),
-        body: Container(
-          color: Colors.white,
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextField(
-                controller: _emailController,
-                keyboardType: TextInputType.emailAddress,
-                decoration: const InputDecoration(
-                  labelText: 'البريد الإلكتروني الجديد',
-                  prefixIcon: Icon(FontAwesomeIcons.envelope),
-                ),
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () async {
-                  await updateEmail();
-                },
-                style: ElevatedButton.styleFrom(
-                 
-                  backgroundColor: YourStoryStyle.titleColor,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(50),
-                  ),
-                  ),
-                
-                child: const Text(
-                  'حفظ',
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  Future<void> updateEmail() async {
-    try {
-      // Get the new email from the text field
-      final String newEmail = _emailController.text.trim();
-
-      // Update the email in Firebase Auth
-      await FirebaseAuth.instance.currentUser!.updateEmail(newEmail);
-
-      // Update the email in Firebase Firestore
-      await FirebaseFirestore.instance.collection("User").doc(userId).update({
-        'email': newEmail,
-      });
-
-      // Show a success message
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text('تم تحديث البريد الإلكتروني بنجاح'),
-        ),
-      );
-
-      // Navigate to the profile page
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => ProfileScreen()),
-      );
-    } catch (e) {
-      // show an error message
-      ScaffoldMessenger.of(context).showSnackBar(
-         CustomSnackBar(
-          content: 'حدث خطأ أثناء تحديث البريد الإلكتروني',
-        ),
-      );
-      print(e);
-    }
-  }
-}
-
 class EditUsernamePage extends StatefulWidget {
   @override
   _EditUsernamePageState createState() => _EditUsernamePageState();
 }
 
 class _EditUsernamePageState extends State<EditUsernamePage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final TextEditingController _usernameController = TextEditingController();
   final String userId = FirebaseAuth.instance.currentUser!.uid;
 
@@ -253,34 +146,62 @@ class _EditUsernamePageState extends State<EditUsernamePage> {
         body: Container(
           color: Colors.white,
           padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextField(
-                controller: _usernameController,
-                decoration: const InputDecoration(
-                          labelText: "الاسم الجديد",
-                          prefixIcon: Icon(
-                            FontAwesomeIcons.user,
-                          ))
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () async {
-                  await updateUsername();
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: YourStoryStyle.titleColor,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(50),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start, // Align input at the top
+              children: [
+                TextFormField(
+                  controller: _usernameController,
+                  textAlign: TextAlign.start,
+                  decoration: const InputDecoration(
+                    labelText: "الاسم الجديد",
+                    prefixIcon: Icon(
+                      FontAwesomeIcons.user,
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty || _usernameController.text.trim() == "") {
+                      return "الحقل مطلوب";
+                    } else if (value.length == 1) {
+                      return " يجب أن يحتوي الاسم أكثر من حرف على الأقل";
+                    } else if (!RegExp(
+                            r"^[\u0600-\u065F\u066A-\u06EF\u06FA-\u06FFa-zA-Z]+(?:\s[\u0600-\u065F\u066A-\u06EF\u06FA-\u06FFa-zA-Z]+)?$")
+                        .hasMatch(value.trim())) {
+                      return 'أدخل اسم يحتوي على أحرف فقط';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () async {
+                    if (_formKey.currentState!.validate()) {
+                      await updateUsername();
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: YourStoryStyle.titleColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50),
+                    ), minimumSize: const Size(180, 40),
+                  ),
+                  child: const Row(
+                   mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+      
+                        Text(
+                          'حفظ',
+                          style: TextStyle(color: Colors.white),
+                          ),
+                          SizedBox(width: 8),
+                          Icon(FontAwesomeIcons.check, color: Colors.white), // Add your desired icon
+                           // Add some space between the icon and text
+                   ],
                   ),
                 ),
-                child: const Text(
-                  'حفظ',
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
@@ -289,9 +210,7 @@ class _EditUsernamePageState extends State<EditUsernamePage> {
 
   Future<void> updateUsername() async {
     try {
-      // Get the new username from the text field
       final String newUsername = _usernameController.text.trim();
-
       // Update the username in Firebase Firestore
       await FirebaseFirestore.instance.collection("User").doc(userId).update({
         'name': newUsername,
@@ -319,91 +238,3 @@ class _EditUsernamePageState extends State<EditUsernamePage> {
     }
   }
 }
-
-
-// import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:flutter/material.dart';
-// import '../../style.dart';
-// import '../../alerts.dart';
-
-// class ProfileScreen extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Directionality(
-//       textDirection: TextDirection.rtl,
-//       child: Scaffold(
-//         appBar: AppBar(
-//           backgroundColor: Colors.white,
-//           title: const Text(
-//             'معلومات الحساب',
-//             style: TextStyle(color: Colors.black),
-//           ),
-//           titleTextStyle: const TextStyle(fontSize: 24,),
-//           centerTitle: true,
-//           leading: const BackButton(color: Colors.black),
-//         ),
-//         body: profileInformation(),
-//       ),
-//     );
-//   }
-// }
-
-// class profileInformation extends StatefulWidget {
-//   @override
-//   _profileInformationState createState() => _profileInformationState();
-// }
-
-// class _profileInformationState extends State<profileInformation> {
-//   final String userId = FirebaseAuth.instance.currentUser!.uid;
-
-//   Future<Map<String, dynamic>?> fetchUserInfo() async {
-//     try {
-//       DocumentSnapshot<Map<String, dynamic>> ds =
-//           await FirebaseFirestore.instance.collection("User").doc(userId).get();
-//       return ds.data();
-//     } catch (e) {
-//       // Handle the error or log it
-//       ScaffoldMessenger.of(context).showSnackBar(
-//         CustomSnackBar(
-//           content: "المعذرة، حصل خطأ",
-//         ),
-//       );
-//       return null;
-//     }
-//   }
-
-//   Widget customListTile(IconData icon, String title, String subtitle) {
-//     return ListTile(
-//       leading: Icon(icon, color: Color.fromARGB(255, 22, 22, 22)),
-//       title: Text(title),
-//       subtitle: Text(subtitle),
-//     );
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return FutureBuilder<Map<String, dynamic>?>(
-//       future: fetchUserInfo(),
-//       builder: (context, snapshot) {
-//         if (snapshot.connectionState == ConnectionState.waiting) {
-//           return Center(
-//               child: CircularProgressIndicator(
-//             color: YourStoryStyle.titleColor,
-//           ));
-//         } else if (snapshot.hasError || !snapshot.hasData) {
-//           return Container();
-//         } else {
-//           String username = snapshot.data?['name'] ?? 'N/A';
-//           String useremail = snapshot.data?['email'] ?? 'N/A';
-//           return ListView(
-//             children: <Widget>[
-//               customListTile(Icons.person, "الإسم", username),
-//               customListTile(Icons.email, "البريد الإلكتروني", useremail),
-//             ],
-//           );
-//         }
-//       },
-//     );
-//   }
-// }
