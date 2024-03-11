60ffc9bb2b0b9effb587b26b32432a1b144afa0b
HessahAlsubaie <Alsubaiehessa9@gmail.com>
2024-03-11 17:34:38 +0300

    Remove the stop words from the story

--- scoring_system.py
+++ scoring_system.py
@@ -16,7 +16,6 @@ stemmer = ISRIStemmer()
 # Initialize Stanza pipeline for Arabic text processing
 nlp = stanza.Pipeline(lang='ar', processors='tokenize,pos')
 
-
 # Define the Flask route for TOPSIS calculation
 @app.route('/calculate_topsis', methods=['POST'])
 def calculate_topsis():
@@ -100,32 +99,14 @@ def calculate_topsis():
 
         # If any stemmed clause token is in the set of stemmed title tokens, return 1, otherwise 0
         return 1 if any(stem in title_stems for stem in clause_stems) else 0
-    
-    # Function to read stop words from a file and return a list of stop words
-    def read_stop_words(file_path):
-        with open(file_path, 'r', encoding='utf-8') as file:
-            stop_words = [line.strip() for line in file]
-        return stop_words
-
-    # Function to remove stop words from a given words list
-    def remove_stop_words(words_list, stop_words):
-        filtered_words = [word for word in words_list if word not in stop_words]
-        return filtered_words
 
     # Function to calculate dissimilarity matrix between clauses in a story
-    def calculate_dissimilarity_matrix(story):
-        # Path to the file containing stop words
-        file_path = 'list.txt'
-
-        # Reading stop words from the file
-        stop_words = read_stop_words(file_path)
-        
+    def calculate_dissimilarity_matrix(story):    
         # Tokenize the story into clauses
         clauses = sentence_tokenize(story)
 
         # Stem the tokens in each clause in the story, excluding punctuation
         stemmed_clauses = [stem_tokens([word.text for sentence in nlp(clause).sentences for word in sentence.words if word.upos != "PUNCT"]) for clause in clauses]
-        stemmed_clauses = [remove_stop_words(clause, stop_words) for clause in stemmed_clauses]
 
         # Get the number of clauses
         n = len(stemmed_clauses)
@@ -203,16 +184,15 @@ def calculate_topsis():
         return normalized_pos_scores
 
 
-
     # Function to process and score clauses in sentences
-    def process_and_score_clauses(sentences, title, nlp, sentiment_model, word_probabilities, title_stems, story):
+    def process_and_score_clauses(filtered_story, filtered_sentences, filtered_title, nlp, sentiment_model, word_probabilities, title_stems, original_story, original_sentences):
         scores_matrix = []  # Initialize an empty matrix to store scores for each clause
         clause_scores = {}  # Initialize an empty dictionary to store scores for each clause
         counter = 0
-        normalized_pos_scores = calculate_pos_scores(sentences, nlp) # Calculate part-of-speech (POS) scores for clauses
+        normalized_pos_scores = calculate_pos_scores(filtered_sentences, nlp) # Calculate part-of-speech (POS) scores for clauses
 
         # Normlaize dissimilarity
-        dissimilarity_matrix = calculate_dissimilarity_matrix(story)  # Calculate dissimilarity matrix for the story
+        dissimilarity_matrix = calculate_dissimilarity_matrix(filtered_story)  # Calculate dissimilarity matrix for the story
         n = len(dissimilarity_matrix)  # Get the number of clauses in the story
         raw_sums = [sum(dissimilarity_matrix[i]) for i in range(n)]  # Calculate raw sums of dissimilarity scores
         min_sum = min(raw_sums)  # Find the minimum raw sum
@@ -220,16 +200,22 @@ def calculate_topsis():
         range_sum = max_sum - min_sum  # Calculate the range of raw sums
         normalized_sums = [(raw_sum - min_sum) / range_sum if range_sum > 0 else 0 for raw_sum in raw_sums]  # Normalize raw sums
 
-        for sentence in sentences:
-            clauses = sentence_tokenize(sentence)  # Tokenize the sentence into clauses          
-            normalized_lengths = calculate_normalized_clause_length(story, clauses)  # Calculate normalized clause lengths
+        # Print the normalized dissimilarity matrix
+        print("Normalized Dissimilarity Matrix:")
+        for row in normalized_sums:
+            print(row)
 
-            for i, clause in enumerate(clauses):
+        for (filtered_sentence, original_sentence) in zip(filtered_sentences, original_sentences):
+            clauses = sentence_tokenize(filtered_sentence)  # Tokenize the sentence into clauses  
+            original_clauses = sentence_tokenize(original_sentence)         
+            normalized_lengths = calculate_normalized_clause_length(filtered_story, clauses)  # Calculate normalized clause lengths
+
+            for i, (clause, original_clause) in enumerate(zip(clauses, original_clauses)):
                 if clause.strip():
                     title_word_score = calculate_title_word_score(clause, title, nlp)  # Calculate title word score for the clause
                     
                     # Sentiment Analysis
-                    sentiment_result = sentiment_model(clause)[0]  # Get sentiment analysis results for the clause
+                    sentiment_result = sentiment_model(original_clause)[0]  # Get sentiment analysis results for the clause
                     sentiment_score = 0
                     sentiment_label = sentiment_result['label']
                     if sentiment_label != 'neutral':
@@ -247,23 +233,34 @@ def calculate_topsis():
                     overall_score = title_word_score + sentiment_score + noun_weight + dissimilarity_score + pos_score + normalized_length  # Calculate the overall score for the clause
                     clause_scores[clause] = overall_score  # Store the overall score in the dictionary
                     scores_matrix.append([sentiment_score, title_word_score, noun_weight, dissimilarity_score, pos_score, normalized_length])  # Append scores to the matrix
+                    print(f"----------------------------------------------------")
+                    print(f"Clause: {clause}")
+                    print(f"Title Word Score: {title_word_score}")
+                    print(f"Sentiment Score: {sentiment_score}")
+                    print(f"Noun Weight: {noun_weight}")
+                    print(f"Dissimilarity Score: {dissimilarity_score}")
+                    print(f"POS Score: {pos_score}")
+                    print(f"Normalized Length Score: {normalized_length}")
+                    print(f"Overall Score for Clause: {clause}: {overall_score}")
+                    print(f"----------------------------------------------------")
 
         return scores_matrix, clause_scores  # Return the scores matrix and clause scores as results
     
     # Function to calculate clause scores for Arabic text
-    def calculate_clause_scores_arabic(story, title):
-        title_doc = nlp(title)  # Process the title using Stanza NLP
+    def calculate_clause_scores_arabic(filtered_story, filtered_title, original_story):
+        title_doc = nlp(filtered_title)  # Process the title using Stanza NLP
         title_tokens = [word.text for sent in title_doc.sentences for word in sent.words]
         title_stems = set(stem_tokens(title_tokens))  # Stem the tokens in the title
 
-        word_probabilities = calculate_word_probability(story)  # Calculate word probabilities for the story
+        word_probabilities = calculate_word_probability(filtered_story)  # Calculate word probabilities for the story
 
         sentiment_model = pipeline('sentiment-analysis', model='CAMeL-Lab/bert-base-arabic-camelbert-mix-sentiment')  # Load a sentiment analysis model for Arabic
 
-        sentences = stanza_sentence_tokenize(story)  # Tokenize the story into sentences
+        filtered_sentences = stanza_sentence_tokenize(filtered_story)  # Tokenize the story into sentences
+        original_sentences = stanza_sentence_tokenize(story) 
 
         # Process and score clauses in the story
-        scores_matrix, clause_scores = process_and_score_clauses(sentences, title, nlp, sentiment_model, word_probabilities, title_stems, story)
+        scores_matrix, clause_scores = process_and_score_clauses(filtered_story, filtered_sentences, filtered_title, nlp, sentiment_model, word_probabilities, title_stems, original_story, original_sentences)
 
         return scores_matrix, clause_scores  # Return the scores matrix and clause scores as results
 
@@ -324,36 +321,25 @@ def calculate_topsis():
         # Convert relative_closeness to a list
         relative_closeness_list = np.array(relative_closeness).tolist()
 
+        # Tokenize the story into sentences using Stanza
+        sentences = stanza_sentence_tokenize(story)
+
         # Map relative closeness to clauses
+        counter=0
         topsis_scores = {}
-        for i, clause in enumerate(clause_scores.keys()):
-            topsis_scores[clause] = relative_closeness_list[i]
+        for sentence in sentences:
+            clauses = sentence_tokenize(sentence)
+            for i, clause in enumerate(clauses):
+                counter += 1
+                if counter <= len(relative_closeness_list):
+                    topsis_scores[clause] = relative_closeness_list[counter - 1]
+                else:
+                    # Handle the case where counter exceeds the length of relative_closeness_list
+                    print("Warning: Index out of range. Adjust your logic.")
+                    break
 
         return topsis_scores
     
-        # Function to process sentences and return structured data with TOPSIS scores
-    def process_sentences_with_topsis(sentences, topsis_scores):
-        sentences_with_clauses = []
-
-        for sentence in sentences:
-            clauses = sentence_tokenize(sentence)
-            clause_scores = []
-
-            for clause in clauses:
-                score = topsis_scores.get(clause, 0) # Get the TOPSIS score for each clause
-                # Check if the score is NaN and replace it with a string or null
-                if math.isnan(score):
-                  score = "NaN"  
-                clause_scores.append({'clause': clause, 'score': score})
-            
-             # Sort the clauses based on their TOPSIS scores (x[2] == TOPSIS scores in clause_scores)
-            # sorted_clause_scores = sorted(clause_scores, key=lambda x: x['score'], reverse=True)
-            sentences_with_clauses.append({
-                'sentence': sentence,
-                'clauses': clause_scores
-            })
-
-        return sentences_with_clauses
 
     # Get the JSON data from the HTTP request
     data = request.get_json()
@@ -362,12 +348,38 @@ def calculate_topsis():
     story = data['story']
     title = data['title']
 
+    def read_stop_words(file_path):
+        with open(file_path, 'r', encoding='utf-8') as file:
+            stop_words = [line.strip() for line in file]
+        return stop_words
+
+    # Function to remove stop words from text
+    def remove_stop_words(text, stop_words):
+        paragraphs = text.split('\n')  # Split the text into paragraphs
+        filtered_paragraphs = []
+
+        for paragraph in paragraphs:
+            words = paragraph.split()  # Tokenize each paragraph into words
+            filtered_words = [word for word in words if word not in stop_words]
+            filtered_paragraph = ' '.join(filtered_words)  # Join the filtered words back into a string
+            filtered_paragraphs.append(filtered_paragraph)
+
+        return '\n'.join(filtered_paragraphs)
+    
+    file_path = 'list.txt'
+    stop_words = read_stop_words(file_path)
+
+    # Remove stop words
+    filtered_story = remove_stop_words(story, stop_words)
+    filtered_title= remove_stop_words(title, stop_words)
+
     # Remove tashkeel (diacritics) from the 'story' and 'title'
-    story = strip_tashkeel(story)
-    title = strip_tashkeel(title)
+    filtered_story = strip_tashkeel(filtered_story)
+    filtered_title = strip_tashkeel(filtered_title)
+    print(filtered_story)
 
     # Calculate clause scores for the Arabic 'story' and 'title'
-    scores_matrix, clause_scores = calculate_clause_scores_arabic(story, title)
+    scores_matrix, clause_scores = calculate_clause_scores_arabic(filtered_story, filtered_title, story)
 
     # Perform TOPSIS ranking using the scores matrix and clause scores
     topsis_scores = topsis_ranking(np.array(scores_matrix), clause_scores)
@@ -375,14 +387,29 @@ def calculate_topsis():
     # Tokenize the story into sentences
     sentences = stanza_sentence_tokenize(story)
 
-    # Process sentences and get structured data with TOPSIS scores
-    sentences_with_topsis = process_sentences_with_topsis(sentences, topsis_scores)
+    # Sort clauses based on their scores in descending order
+    sorted_clauses = sorted(topsis_scores.items(), key=lambda x: x[1], reverse=True)
 
-    print("Sentences with TOPSIS Scores:", sentences_with_topsis)
+    sentences_with_topsis = []
+
+    for sentence in sentences:
+        clauses = sentence_tokenize(sentence)
+        sentence_with_clauses = {
+            'sentence': sentence,
+            'clauses': []
+        }
+
+        for (clause, score) in sorted_clauses:
+            if clause in clauses:
+                sentence_with_clauses['clauses'].append({
+                    'clause': clause,
+                    'score': score
+                })
+
+        sentences_with_topsis.append(sentence_with_clauses)
 
     # Return the structured data as a JSON response
     return jsonify(sentences_with_topsis)
-
 
 if __name__ == '__main__':
     app.run(host='', debug=True)
