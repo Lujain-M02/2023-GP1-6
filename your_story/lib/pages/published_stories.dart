04ad74083a188768d07cc1b23307bbc6d298e579
nourahmaziad <nourahmaziad@gmail.com>
2024-03-21 06:35:40 +0300

    styling the published stories page part1

--- your_story/lib/pages/published_stories.dart
+++ your_story/lib/pages/published_stories.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:shimmer/shimmer.dart';
 import 'package:your_story/pages/create_story_pages/create_story.dart';
 import 'package:your_story/pages/MyStories_Page/pdf_methods.dart';
@@ -127,7 +129,7 @@ class _StoriesPageState extends State<StoriesPage> {
   //   super.dispose();
   // }
 
-  Widget _buildPdfCard(String title, String pdfUrl, String docId, int index,
+  Widget _buildPdfCard(String title, String pdfUrl, String docId,String fimg, int index,
       BuildContext context) {
     return GestureDetector(
       onTap: () {
@@ -157,9 +159,9 @@ class _StoriesPageState extends State<StoriesPage> {
           children: [
             Expanded(
               child: Image.asset(
-                "assets/6.png",
-              ),
-            ),
+                      "assets/6.png", 
+                          ),
+                  ),                                                 
             Center(
               child: Text(
                 title,
@@ -217,9 +219,8 @@ class _StoriesPageState extends State<StoriesPage> {
       body: StreamBuilder<List<QueryDocumentSnapshot>>(
         stream: _storiesStreamController.stream,
         builder: (context, snapshot) {
-// shimmer effect while refreshing
 
-          /*   if (_isRefreshing) {
+            /*   if (_isRefreshing) {
            return Center(
              child: CircularProgressIndicator(),
            );
@@ -292,7 +293,6 @@ class _StoriesPageState extends State<StoriesPage> {
              ),
            );
            }*/
-
           if (snapshot.connectionState == ConnectionState.waiting) {
             return Container(
               padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
@@ -387,44 +387,198 @@ class _StoriesPageState extends State<StoriesPage> {
           }
 
           final stories = snapshot.data!;
-
-          return SingleChildScrollView(
+          return  SingleChildScrollView(
             child: Container(
-              padding: const EdgeInsets.only(
-                left: 20,
-                top: 30,
-                right: 20,
-                //bottom: 700,
-              ),
-              decoration: const BoxDecoration(
-                color: Color.fromARGB(255, 255, 255, 255),
-                borderRadius: BorderRadius.only(topLeft: Radius.circular(60)),
-              ),
-              child: GridView.builder(
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                padding: const EdgeInsets.all(20),
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 10,
-                  mainAxisSpacing: 10,
-                  childAspectRatio: 0.75,
+                padding: const EdgeInsets.only(
+                  left: 20,                  
+                  right: 20,
                 ),
-                itemCount: stories.length,
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  color: Color.fromARGB(255, 244, 247, 252),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(60),
+                  ),
+                ),
+      child: Column(
+      children: [
+         Padding(
+          padding: const EdgeInsets.only(top: 20,),
+          child: Container(
+          width: double.infinity, 
+    alignment: Alignment.centerRight,
+    child: const Text(
+            "القصص المنشورة حديثا",
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+        ),),
+            Container(
+              height: 200, 
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: stories.length, 
                 itemBuilder: (context, index) {
                   final pdfData =
-                      stories[index].data() as Map<String, dynamic>?;
-                  final String title = pdfData?['title'] ?? 'Untitled';
-                  final String pdfUrl = pdfData?['url'] ?? '#';
-                  final String docId = stories[index].id;
+                        stories[index].data() as Map<String, dynamic>?;
+                    final String title = pdfData?['title'] ?? 'Untitled';
+                    final String pdfUrl = pdfData?['url'] ?? '#';
+                  return  GestureDetector(
+        onTap: () {
+          Navigator.push(
+                     context,
+                     MaterialPageRoute(
+                     builder: (context) => ViewPDFPage(pdfUrl: pdfUrl, storyTitle: title),
+                            )
+                          );
+        },
+        child:  Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: SizedBox(
+                      width: 150,
+                      child: AspectRatio(
+                        aspectRatio: 0.83,
+                        child: Stack(
+                          alignment: Alignment.bottomCenter,
+                          children: <Widget>[
+                            ClipPath(
+                              clipper: CategoryCustomShape(),
+                              child: AspectRatio(
+                                aspectRatio: 1.025,
+                                child: Container(
+                                  color: kSecondaryColor, 
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    children: <Widget>[    
+                                      Text(title, style: const TextStyle(fontSize: 15)),
+                                      Row(             
+                children: [
+                  Padding(padding: const EdgeInsets.only(bottom: 0,right: 25,),
+                    child: IconButton(
+                      icon: const Icon(Icons.share,
+                          color: Color.fromARGB(255, 5, 0, 58)),
+                      onPressed: () => sharePdf(pdfUrl, title, context),
+                    ),
+                  ),
+             Padding(padding: const EdgeInsets.only(bottom: 0,),             
+                  child: 
+                    IconButton(
+                      icon: const Icon(
+                        Icons.arrow_downward,
+                      ),
+                      onPressed: () =>
+                          downloadAndSaveFile(pdfUrl, title,context,)
+                    ),
+                  ),
+                ],
+              ),
             
-                  return _buildPdfCard(title, pdfUrl, docId, index, context);
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Positioned(
+                              top: 0,
+                              left: 0,
+                              right: 0,
+                              child: AspectRatio(
+                                aspectRatio: 1.34,
+                                child: Image.asset(
+                                  "assets/6.png", 
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ));
                 },
               ),
+            ),
+              Padding(
+          padding: const EdgeInsets.all(5),
+          child: Container(
+    width: double.infinity, 
+    alignment: Alignment.centerRight,
+    child: const Text(
+      "جميع القصص", 
+      style: TextStyle(
+        fontSize: 18,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+          ),
+        ),         
+                 GridView.builder(
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      padding: const EdgeInsets.all(10),
+                      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 10,
+                        mainAxisSpacing: 10,
+                        childAspectRatio: 0.75,
+                      ),
+                      itemCount: stories.length,
+                      itemBuilder: (context, index) {
+                        final pdfData =
+                            stories[index].data() as Map<String, dynamic>?;
+                        final String title = pdfData?['title'] ?? 'Untitled';
+                        final String pdfUrl = pdfData?['url'] ?? '#';
+                        final String docId = stories[index].id;
+                        final String fimg = pdfData?['imageUrl'] ?? '#';
+
+                  
+                        return _buildPdfCard(title, pdfUrl, docId,fimg, index, context);
+                      },
+                    ),
+              
+            
+              ]),
             ),
           );
         },
       ),
+
+      //     return SingleChildScrollView(
+      //       child: Container(
+      //         padding: const EdgeInsets.only(
+      //           left: 20,
+      //           top: 30,
+      //           right: 20,
+      //           //bottom: 700,
+      //         ),
+      //         decoration: const BoxDecoration(
+      //           color: Color.fromARGB(255, 255, 255, 255),
+      //           borderRadius: BorderRadius.only(topLeft: Radius.circular(60)),
+      //         ),
+      //         child: GridView.builder(
+      //           shrinkWrap: true,
+      //           physics: const NeverScrollableScrollPhysics(),
+      //           padding: const EdgeInsets.all(20),
+      //           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+      //             crossAxisCount: 2,
+      //             crossAxisSpacing: 10,
+      //             mainAxisSpacing: 10,
+      //             childAspectRatio: 0.75,
+      //           ),
+      //           itemCount: stories.length,
+      //           itemBuilder: (context, index) {
+      //             final pdfData =
+      //                 stories[index].data() as Map<String, dynamic>?;
+      //             final String title = pdfData?['title'] ?? 'Untitled';
+      //             final String pdfUrl = pdfData?['url'] ?? '#';
+      //             final String docId = stories[index].id;
+            
+      //             return _buildPdfCard(title, pdfUrl, docId, index, context);
+      //           },
+      //         ),
+      //       ),
+      //     );
+      //   },
+      // ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push(
@@ -438,3 +592,31 @@ class _StoriesPageState extends State<StoriesPage> {
     );
   }
 }
+
+class CategoryCustomShape extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    double height = size.height;
+    double width = size.width;
+    double cornerSize = 30;
+
+    path.lineTo(0, height - cornerSize);
+    path.quadraticBezierTo(0, height, cornerSize, height);
+    path.lineTo(width - cornerSize, height);
+    path.quadraticBezierTo(width, height, width, height - cornerSize);
+    path.lineTo(width, cornerSize);
+    path.quadraticBezierTo(width, 0, width - cornerSize, 0);
+    path.lineTo(cornerSize, cornerSize * 0.75);
+    path.quadraticBezierTo(0, cornerSize, 0, cornerSize * 2);
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return true;
+  }
+}
+
