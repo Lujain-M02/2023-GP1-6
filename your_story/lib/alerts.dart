ca7358e7521f89f378b20e064d8ab9c328a5d3cf
Lujain-M02 <ilujain321@gmail.com>
2024-05-16 10:39:46 +0300

    remove comments from "alerts"

--- your_story/lib/alerts.dart
+++ your_story/lib/alerts.dart
@@ -214,20 +214,6 @@ class NumberPickerAlertDialog {
                           const Text("متأكد", style: TextStyle(fontSize: 20)),
                     ),
                   ),
-                  // OutlinedButton(
-                  //   onPressed: () => Navigator.of(context).pop(),
-                  //   style: OutlinedButton.styleFrom(
-                  //     foregroundColor: Colors.black,
-                  //     side: BorderSide(
-                  //       color: YourStoryStyle.primarycolor,
-                  //       width: 2,
-                  //     ),
-                  //     shape: RoundedRectangleBorder(
-                  //       borderRadius: BorderRadius.circular(15.0),
-                  //     ),
-                  //   ),
-                  //   child: const Text("الغاء", style: TextStyle(fontSize: 20)),
-                  // ),
                 ],
               ),
             );
